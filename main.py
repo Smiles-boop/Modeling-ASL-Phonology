84fb234a02caab17130daf6e2f8b1bc24555b66b
Smiles-boop <67544383+Smiles-boop@users.noreply.github.com>
2023-08-07 10:56:29 +1000

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import omegaconf
+from openhands.apis.inference import InferenceModel
 from openhands.apis.classification_model import ClassificationModel
 from openhands.core.exp_utils import get_trainer
 
@@ -8,5 +9,12 @@ trainer = get_trainer(cfg)
 model = ClassificationModel(cfg=cfg, trainer=trainer)
 model.init_from_checkpoint_if_available()
 model.fit()
+
+cfg = omegaconf.OmegaConf.load("videos", inference_mode = True)
+if cfg.data.test_pipeline.dataset.inference_mode:
+    model.test_inference()
+else:
+    model.compute_test_accuracy()
+
 
 
