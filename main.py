9794aff82a8fba7061c7cc321464263315146b22
Smiles-boop <67544383+Smiles-boop@users.noreply.github.com>
2023-08-10 14:36:58 +1000

    running model

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import omegaconf
-from openhands.apis.inference import InferenceModel
-from openhands.apis.classification_model import ClassificationModel
-from openhands.core.exp_utils import get_trainer
+import torch.nn as nn
+import omegaconf 
+from openhands.apis.inference import InferenceModel 
 
-cfg = omegaconf.OmegaConf.load("gcn_adapter_train.yaml")
-trainer = get_trainer(cfg)
-
-model = ClassificationModel(cfg=cfg, trainer=trainer)
+cfg = omegaconf.OmegaConf.load("gcn_base_all_test.yaml")
+model = InferenceModel(cfg=cfg)
 model.init_from_checkpoint_if_available()
-model.fit()
-
-cfg = omegaconf.OmegaConf.load("videos", inference_mode = True)
 if cfg.data.test_pipeline.dataset.inference_mode:
     model.test_inference()
-else:
-    model.compute_test_accuracy()
-
-
-
